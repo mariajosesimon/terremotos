97e87cef26880d60f60805e53fbdbdccbe4c5f93
Mª Jose Simón <mariajose.simon@ikasle.egibide.org>
2021-02-05 19:05:38 +0100

    1. Creados los 2 gitignore

---
+++ terremotos/terremotos/terremotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  terremotosApp.swift
+//  terremotos
+//
+//  Created by  on 05/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct terremotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
